611667b1d7f1d4fc5aba2e70576961d4e169b782
girish0987 <girishsvt143>
2022-09-15 16:01:42 +0530

    dom3Commit

--- dom.js
+++ dom.js
@@ -1,4 +1,7 @@
-var li=document.getElementsByTagName('li');
-console.log(li);
-li[2].style.backgroundColor='green';
-
+var items=document.querySelectorAll('.list-group-item');
+console.log(items);
+items[1].style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.background='green';
+}
