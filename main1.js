9e024b27ac8c4f5825ea6b6b51a9a21edbedc4bb
girish0987 <girishsvt143>
2022-09-19 15:48:46 +0530

    localstorage3

--- main1.js
+++ main1.js
@@ -10,6 +10,13 @@ function addDetails (e){
     }
     
     let myobj_1=JSON.stringify(myobj);
-    localStorage.setItem('details',myobj_1)
-    console.log(myobj_1);
+    localStorage.setItem(myobj.email,myobj_1)
+    
+    localStorage.getItem(myobj.email);
+    showNewUserOnScreen(myobj);
+}
+function showNewUserOnScreen(user){
+    const parentNode=document.getElementById('list of users');
+    childHTML=`<li>${user.name}-${user.email}</li>`;
+    parentNode.innerHTML=parentNode.innerHTML+childHTML;
 }
